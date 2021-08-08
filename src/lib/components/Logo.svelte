15157c67e60120ec2f696c5cba28cbe1c5ebf842
Brian Ketelsen <bketelsen@gmail.com>
2021-08-08 09:09:41 -0400

    more style

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -2,9 +2,11 @@
   export let title
 </script>
 
-<svg aria-label={title} length="auto" height="30">
-  <text x="0" y="20">{title}</text>
-</svg>
+<span class="text-primary"
+  ><svg aria-label={title} length="auto" height="30">
+    <text x="0" y="20">{title}</text>
+  </svg>
+</span>
 
 <style>
   text {
